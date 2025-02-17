767170329cf5c0ae8a14c14a360ab18a842b1677
Pedro Augusto <pedro.sampaio@sps.ce.gov.br>
2025-02-17 11:04:24 -0300

    Fundamentos do Nest: Configurando autenticação JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from 'env'
+import { envSchema } from 'src/env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { envSchema } from 'env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
