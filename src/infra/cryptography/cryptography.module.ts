738e3b627a67579af8040fde9c635d2ba2a1a164
Pedro Augusto <pedro.sampaio@sps.ce.gov.br>
2025-02-20 10:34:26 -0300

    Desacoplando camadas no NestJS: Implementação da Criptografia

---
+++ src/infra/cryptography/cryptography.module.ts
@@ -0,0 +1,16 @@
+import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
+import { Module } from '@nestjs/common'
+import { JwtEncrypter } from './jwt-encrypter'
+import { HashComparer } from '@/domain/forum/application/cryptography/hasher-comparer'
+import { BcryptHasher } from './bcrypt-hasher'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hasher-generator'
+
+@Module({
+  providers: [
+    { provide: Encrypter, useClass: JwtEncrypter },
+    { provide: HashComparer, useClass: BcryptHasher },
+    { provide: HashGenerator, useClass: BcryptHasher },
+  ],
+  exports: [Encrypter, HashComparer, HashGenerator],
+})
+export class CrypographyModule {}
