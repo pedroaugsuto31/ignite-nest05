0c07d12966aeba065056c75f656f15d8522d0f54
Pedro Augusto <pedro.sampaio@sps.ce.gov.br>
2025-02-20 10:50:50 -0300

    Desacoplando camadas no NestJS: Refatorando controller de Autenticação

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common'
 
-import { CreateAccountController } from './controllers/create-account.controller'
-import { AuthenticateController } from './controllers/authenticate.controller'
-import { CreateQuestionController } from './controllers/create-question.controller'
-import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { DatabaseModule } from '../database/database.module'
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
+import { CrypographyModule } from '../cryptography/cryptography.module'
+import { DatabaseModule } from '../database/database.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CrypographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+  ],
 })
 export class HttpModule {}
