ae485dfd948890394ca5ef010dd8c1c6e570553d
Pedro Augusto <pedro.sampaio@sps.ce.gov.br>
2025-03-12 14:30:32 -0300

    Uploads e Relacionamentos: Controller comentário com autor

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -8,7 +8,7 @@ import {
   Query,
 } from '@nestjs/common'
 import { z } from 'zod'
-import { CommentPresenter } from '../presenters/cooment-presenter'
+import { CommentWithAuthorPresenter } from '../presenters/coment-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -39,8 +39,8 @@ export class FetchQuestionCommentsController {
       throw new BadRequestException()
     }
 
-    const questionComments = result.value.questionComments
+    const comments = result.value.comments
 
-    return { comments: questionComments.map(CommentPresenter.toHTTP) }
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
